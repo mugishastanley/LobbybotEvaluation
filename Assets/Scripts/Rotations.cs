89f5bb6e6bf9b1cf7f569362565c8e239a34f9d0
Stanley <stanley.mugisha@ls2n.fr>
2021-03-01 14:49:35 +0100

    Changed Prop Orientation. prop offset needs to be completed.

---
+++ Assets/Scripts/Rotations.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotations : MonoBehaviour
+{
+    /**This script defines rotations manually coded
+     steering = (1.25, 0.514, -1.291) or (71.61972, 30, -73.974 )
+     screen = (1.088, 0.417, -1.128) or (62.337808,27.04361,-64.171)
+     Home = (2.34, 0.5, 2.34) or (134.1, 30, 134) or RPY (107, -85, 92.29)
+    
+        
+     */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
